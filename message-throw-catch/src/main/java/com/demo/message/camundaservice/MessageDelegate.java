fbf6fe55ba6d7364eebdc22e81f7491b11572d60
SayanDatta27 <47360260+SayanDatta27@users.noreply.github.com>
2020-04-13 20:42:34 +0530

    Add files via upload

---
+++ message-throw-catch/src/main/java/com/demo/message/camundaservice/MessageDelegate.java
@@ -0,0 +1,15 @@
+package com.demo.message.camundaservice;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class MessageDelegate implements JavaDelegate{
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		String question=(String)execution.getVariable("question");
+		execution.getProcessEngineServices().getRuntimeService().createMessageCorrelation("AskGoogle").setVariable("question", question).correlate();
+		
+	}
+
+}
