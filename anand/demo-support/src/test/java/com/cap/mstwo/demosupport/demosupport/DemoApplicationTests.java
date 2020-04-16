688f608d8c287ca90646d4cf0e1f159ece95a4a7
Priyadarshee <anand.priyadarshee@aig.com>
2020-04-16 00:38:19 -0400

    baselined the project directory structure

---
+++ anand/demo-support/src/test/java/com/cap/mstwo/demosupport/demosupport/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cap.mstwo.demosupport.demosupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
