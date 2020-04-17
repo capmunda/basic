24e0080e316f5273373926fe72b6ed14749e7a8b
SayanDatta27 <47360260+SayanDatta27@users.noreply.github.com>
2020-04-17 19:43:17 +0530

    Add files via upload

---
+++ microservice-parent/src/test/java/com/demo/message/MessageThrowCatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageThrowCatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
