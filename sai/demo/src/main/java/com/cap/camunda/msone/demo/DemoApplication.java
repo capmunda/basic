54b9a00f5fa26936dc396773158abadd774c09f7
Sai-Ghorpade <saigunranjan.ghorpade@capgemini.com>
2020-04-20 15:43:00 +0530

    projects baselined

---
+++ sai/demo/src/main/java/com/cap/camunda/msone/demo/DemoApplication.java
@@ -0,0 +1,62 @@
+package com.cap.camunda.msone.demo;
+
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cap.camunda.msone.demo.utility.RestUtility;
+
+@SpringBootApplication
+public class DemoApplication {
+	private final static Logger LOGGER = Logger.getLogger(DemoApplication.class.getName());
+	
+	
+	
+	public static void main(final String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		    
+		    ExternalTaskClient client = ExternalTaskClient.create().baseUrl("http://localhost:8180/CamundaWorkflow/rest/engine/default")
+					.asyncResponseTimeout(10000) // long polling timeout
+					.build();
+
+		    //Task-1 Call activity with external task
+		    //BPMN WeatherCheck.bpmn (parent) printForecast.bpmn(child)
+			// subscribe to an external task topic as specified in the process
+			client.subscribe("forecast").lockDuration(1000) // the default lock duration is 20 seconds, but you can
+			// override this
+			.handler((externalTask, externalTaskService) -> {
+			String day=externalTask.getVariable("day");
+			
+			LOGGER.info("\n Day:"+ day +"\n weather is awesome\n" + new Timestamp(new Date().getTime()));
+				externalTaskService.complete(externalTask);
+			}).open();
+			
+			
+			//Task-3 Spin out new process from external task
+			//BPMN externalChildTrigger.bpmn(parent) childProcess.bpmn(child)
+			// subscribe to an external task topic as specified in the process
+			RestUtility restCall = new RestUtility();
+			client.subscribe("triggerChild").lockDuration(1000) // the default lock duration is 20 seconds, but you can
+					// override this
+					.handler((externalTask, externalTaskService) -> {
+						//String day = externalTask.getVariable("day");
+						restCall.triggerChild();
+						LOGGER.info("\n In external task \n" + new Timestamp(new Date().getTime()));
+						externalTaskService.complete(externalTask);
+					}).open();
+
+		  }
+	}
+
+// 	@Override
+// public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+//     registry.addResourceHandler("/resources/**")
+//             .addResourceLocations("/resources/", "file:resources/");
+// }
+
+
---
+++ sai/demo/src/main/java/com/cap/camunda/msone/demo/utility/RestUtility.java
@@ -0,0 +1,40 @@
+package com.cap.camunda.msone.demo.utility;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class RestUtility {
+	public void triggerChild() {
+		String url = "http://localhost:8180/CamundaWorkflow/rest/engine/default/message";
+
+		// create an instance of RestTemplate
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		// set `content-type` header
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		// set `accept` header
+		headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+		// request body parameters
+		Map<String, Object> map = new HashMap<>();
+		map.put("messageName", "OrderDone");
+		map.put("businessKey", "2");
+
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<Map<String, Object>>(map, headers);
+		// send POST request
+		ResponseEntity<String> response = restTemplate.postForEntity(url, entity, String.class);
+		System.out.println(response.getStatusCode());
+		// check response
+		if (response.getStatusCode() == HttpStatus.NO_CONTENT) {
+		    System.out.println("Request Successful");
+		} else {
+		    System.out.println("Request Failed");
+		}	}
+}
