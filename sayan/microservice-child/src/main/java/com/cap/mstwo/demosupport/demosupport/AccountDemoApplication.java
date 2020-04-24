419c8cf8dc7db8bb7f2bd4c6ec2d6029bcf93375
Sayan Datta <sayadatt@ODC.COM>
2020-04-24 20:43:48 +0530

    Bpmn and subfolders have added

--- sayan/microservice-child/src/main/java/com/cap/mstwo/demosupport/demosupport/AccountDemoApplication.java
+++ sayan/microservice-child/src/main/java/com/cap/mstwo/demosupport/demosupport/AccountDemoApplication.java
@@ -22,38 +22,6 @@ public class AccountDemoApplication {
 		ExternalTaskDemo extd=new ExternalTaskDemo();
 		extd.addNumbers();
 		extd.multiplynumbers();
-		
-		/*ExternalTaskClient client = ExternalTaskClient.create().baseUrl("http://localhost:8080/engine-rest")
-				.asyncResponseTimeout(10000) // long polling timeout
-				.build();
-
-		// subscribe to an external task topic as specified in the process
-		client.subscribe("deposit-money").lockDuration(1000) // the default lock duration is 20 seconds, but you can
-		// override this
-		.handler((externalTask, externalTaskService) -> {
-			long amount=externalTask.getVariable("amount");
-			LOGGER.info("the sum of  " +amount+" has been credited successfully at "+ new Timestamp(new Date().getTime()));
-			externalTaskService.complete(externalTask);
-		}).open();
-		
-		client.subscribe("withdraw-money").lockDuration(1000) // the default lock duration is 20 seconds, but you can
-		// override this
-		.handler((externalTask, externalTaskService) -> {
-			 amount=externalTask.getVariable("amount");
-			LOGGER.info("the sum of  " +amount+" has been debited successfully at " +new Timestamp(new Date().getTime()));
-			new AccountDemoApplication().setAmount(amount);
-			externalTaskService.complete(externalTask);
-		}).open();
-		System.out.println(getAmount());
-		
-	}
-	public static long getAmount() {
-		return amount;
-		
-	}
-	public  void setAmount(long amount) {
-		this.amount=amount;
-	}
-*/
+	
 	}
 }
